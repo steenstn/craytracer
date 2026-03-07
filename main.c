23dcd047ac758e0f0136b36da156e3a0ac4d1140
steenstn <alest849@gmail.com>
2026-03-07 11:00:59 +0100

    Continuous rendering

--- main.c
+++ main.c
@@ -21,7 +21,6 @@ typedef struct Sphere {
 // For the file
 unsigned char image_data[IMAGE_WIDTH * IMAGE_HEIGHT * 3];
 
-
 float picture[IMAGE_WIDTH][IMAGE_HEIGHT][3];
 
 const int numRays = 10; // Rays per iteration
@@ -41,48 +40,55 @@ int main(void) {
     srand(0);
     Vector s = {0,0,0};
     float xmax = 5, ymax = 5;
-    int image_index = 0;
-    for(int screenY = 0; screenY < IMAGE_HEIGHT; screenY++) {
-        for(int screenX = 0; screenX < IMAGE_WIDTH; screenX++) {
-            Vector end_color = {};
-            float x, y;
-            x = (float)(screenX * 6) / (float)IMAGE_WIDTH - 3.0;
-            y = (float)(screenY * 6) * (float)IMAGE_HEIGHT / (float)IMAGE_WIDTH /
-                (float)IMAGE_HEIGHT - 3.0 * (float)IMAGE_HEIGHT / (float)IMAGE_WIDTH;
+    int num_passes = 0;
+    while(true) {
 
-            Vector dir =  {.x = x / xmax, .y = y / ymax, .z = -1};
-            dir = vector_normalize(dir);
+        for(int screenY = 0; screenY < IMAGE_HEIGHT; screenY++) {
+            for(int screenX = 0; screenX < IMAGE_WIDTH; screenX++) {
+                Vector end_color = {};
+                float x, y;
+                x = (float)(screenX * 6) / (float)IMAGE_WIDTH - 3.0;
+                y = (float)(screenY * 6) * (float)IMAGE_HEIGHT / (float)IMAGE_WIDTH / (float)IMAGE_HEIGHT - 3.0 * (float)IMAGE_HEIGHT / (float)IMAGE_WIDTH;
 
-            for(int i = 0; i < numRays; i++) {
-                // bounces = 0
-                
-                end_color = vector_plus(end_color, shoot_ray(all_spheres, 2, s, dir));
+                Vector dir =  {.x = x / xmax, .y = y / ymax, .z = -1};
+                dir = vector_normalize(dir);
+
+                for(int i = 0; i < numRays; i++) {
+                    // bounces = 0
+                    
+                    end_color = vector_plus(end_color, shoot_ray(all_spheres, 2, s, dir));
+                    //printf("%f %f %f\n", end_color.x, end_color.y, end_color.z);
+                }
+                end_color = vector_dividef(end_color, (float)numRays);
                 //printf("%f %f %f\n", end_color.x, end_color.y, end_color.z);
+
+                picture[screenX][screenY][0] += end_color.x;
+                picture[screenX][screenY][1] += end_color.y;
+                picture[screenX][screenY][2] += end_color.z;
             }
-            end_color = vector_dividef(end_color, (float)numRays);
-            printf("%f %f %f\n", end_color.x, end_color.y, end_color.z);
 
-            picture[screenX][screenY][0] = end_color.x;
-            picture[screenX][screenY][1] = end_color.y;
-            picture[screenX][screenY][2] = end_color.z;
         }
-    }
-    for(int screenY = 0; screenY < IMAGE_HEIGHT; screenY++) {
-        for(int screenX = 0; screenX < IMAGE_WIDTH; screenX++) {
-            float r = picture[screenX][screenY][0];
-            float g = picture[screenX][screenY][1];
-            float b = picture[screenX][screenY][2];
-            short end_r = clamp(r,0,1) * 255;
-            short end_g = clamp(g,0,1) * 255;
-            short end_b = clamp(b,0,1) * 255;
-            image_data[image_index] = (unsigned char)end_r;
-            image_data[image_index+1] = (unsigned char)end_g;
-            image_data[image_index+2] = (unsigned char)end_b;
-            image_index+=3;
-        }
-    }
 
-   save_image(image_data, "result.ppm",800,600);
+        num_passes++;
+        int image_index = 0;
+        for(int screenY = 0; screenY < IMAGE_HEIGHT; screenY++) {
+            for(int screenX = 0; screenX < IMAGE_WIDTH; screenX++) {
+                float r = picture[screenX][screenY][0];
+                float g = picture[screenX][screenY][1];
+                float b = picture[screenX][screenY][2];
+                short end_r = clamp(r* 255/num_passes, 0, 255);
+                short end_g = clamp(g* 255/num_passes, 0, 255);
+                short end_b = clamp(b* 255/num_passes, 0, 255);
+                image_data[image_index] = (unsigned char)end_r;
+                image_data[image_index+1] = (unsigned char)end_g;
+                image_data[image_index+2] = (unsigned char)end_b;
+                image_index+=3;
+            }
+        }
+    
+        printf("Saving image...\n");
+        save_image(image_data, "result.ppm",800,600);
+    }
 
 }
 
@@ -90,13 +96,6 @@ Vector shoot_ray(Sphere *spheres, int num_spheres, Vector start, Vector directio
   int max_bounces = 50; 
   Vector resulting_color = {};
   Vector throughput = {1.0, 1.0, 1.0};
- /* 
-    ObjectHit hit = ray_sphere_intersection(all_spheres, num_spheres, start, direction);
-    if (hit.index != -1) {
-        return (Vector){1.0,1.0,1.0};
-    }
-    else return (Vector){};
-    */
 
   for(int num_bounces = 0; num_bounces < max_bounces; num_bounces++) {
     ObjectHit hit = ray_sphere_intersection(all_spheres, num_spheres, start, direction);
@@ -105,24 +104,35 @@ Vector shoot_ray(Sphere *spheres, int num_spheres, Vector start, Vector directio
       break;
     }
 
-    Vector new_direction = {.x = 2 * makeRandom() - 1, .y=2 * makeRandom() -1, .z=2*makeRandom() -1};
-
     Sphere hit_sphere = all_spheres[hit.index];
    
     Vector hit_normal = vector_minus(hit.position, hit_sphere.position);
     hit_normal = vector_normalize(hit_normal);
-    new_direction = vector_cross(new_direction, hit_normal);
-    new_direction = vector_normalize(new_direction);
+    Vector tangent;
 
+    if (fabs(hit_normal.x) > 0.1)
+        tangent = (Vector){0,1,0};
+    else
+        tangent = (Vector){1,0,0};
+
+    Vector bitangent = vector_normalize(vector_cross(hit_normal, tangent));
+    tangent = vector_normalize(vector_cross(bitangent, hit_normal));
     float eps1 = makeRandom() * 6.28318; // 2*PI
     float eps2 = sqrtf(makeRandom());
 
     float x = cosf(eps1) * eps2;
     float y = sinf(eps1) * eps2;
     float z = sqrtf(1.0f - eps2 * eps2);
-    Vector ssx = vector_plus(vector_plus(vector_multiplyf(new_direction, x), vector_multiplyf(vector_cross(hit_normal,new_direction), y)), vector_multiplyf(hit_normal, z));
-    ssx = vector_normalize(ssx);
-    direction = ssx;
+    direction =
+        vector_plus(
+            vector_plus(
+                vector_multiplyf(tangent, x),
+                vector_multiplyf(bitangent, y)
+            ),
+            vector_multiplyf(hit_normal, z)
+        );
+
+    direction = vector_normalize(direction);
     start = vector_plus(hit.position, vector_multiplyf(hit_normal, 0.001f));
 
     Vector this_color = {0.3,0.8,0.1};//Vector(all_colors[hit.index].r, all_colors[hit.index].g, all_colors[hit.index].b);
