de6260e61bc43c29f6063a5e129961ccec09c70b
steenstn <alest849@gmail.com>
2026-03-18 22:17:52 +0100

    First draft of saving scene

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_video.h>
 #include <math.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -28,15 +29,19 @@ float picture[IMAGE_WIDTH*IMAGE_HEIGHT*3];
 
 const int numRays = 1; // Rays per iteration
 
-Sphere all_spheres[NUM_SPHERES];
-Vector all_colors[NUM_SPHERES];
+typedef struct Scene {
+    Sphere all_spheres[NUM_SPHERES];
+    Vector all_colors[NUM_SPHERES];
+    Vector s;
+} Scene;
 
 void print_vector(Vector v) {
     printf("x: %f y: %f z: %f\n", v.x, v.y, v.z);
 }
 
-int main(void) {
+int main(int argc, char* argv[]) {
 
+    Scene scene;
     if(SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL could not initialize: %s\n", SDL_GetError());
         return 1;
@@ -55,40 +60,52 @@ int main(void) {
     bool quit = false;
 
     srand(omp_get_wtime());
-    Vector white = (Vector){1.0,1.0,1.0};
-    Vector color_1 = (Vector){0.6,0.1,0.4};
-    Vector color_2 = (Vector){0.3,0.1,0.8};
-    color_1 = vector_dividef(vector_plus(color_1, white), 2);
-    color_2 = vector_dividef(vector_plus(color_2, white), 2);
 
-    int num_start_spheres = 10;
+    if (argc == 2) {
+        printf("Loading %s\n", argv[1]);
+        FILE *file;
+        file = fopen("scene.bin", "rb");
+        size_t res = fread(&scene, sizeof(scene), 1, file);
+        printf("Read %zu\n", res);
+        fclose(file);
+    } else {
+        Vector white = (Vector){1.0,1.0,1.0};
+        Vector color_1 = (Vector){0.6,0.1,0.4};
+        Vector color_2 = (Vector){0.3,0.1,0.8};
+        color_1 = vector_dividef(vector_plus(color_1, white), 2);
+        color_2 = vector_dividef(vector_plus(color_2, white), 2);
 
-    for(int i = 0; i < num_start_spheres; i++) {
-        all_spheres[i] = (Sphere){.position = {.x = 100*make_random()-50, .y=-1, .z=-70 + 20*make_random()}, .radius=1.0};
-        all_colors[i] = color_1;
+        int num_start_spheres = 10;
+
+        for(int i = 0; i < num_start_spheres; i++) {
+            scene.all_spheres[i] = (Sphere){.position = {.x = 100*make_random()-50, .y=-1, .z=-70 + 20*make_random()}, .radius=1.0};
+            scene.all_colors[i] = color_1;
+        }
+
+
+        for(int i = num_start_spheres; i < NUM_SPHERES-1; i++) {
+            int sphere_to_grow_from_index = floor(make_random()*i);
+
+            Vector direction = (Vector){2*make_random()-1, make_random()*-1, 2*make_random()-1};
+            direction = vector_normalize(direction);
+            Vector new_position = vector_plus(scene.all_spheres[sphere_to_grow_from_index].position, vector_multiplyf(direction,6.0));
+            Vector the_color = make_random() > 0.6 ? color_1 : color_2;
+            scene.all_spheres[i] = (Sphere){.position = new_position, .radius = 2.0};
+            scene.all_colors[i] = the_color;
+        }
+
+        // Big ground sphere
+        scene.all_colors[NUM_SPHERES-1] = (Vector){.x = 0.79, .y = 0.79, .z = 0.79};
+        scene.all_spheres[NUM_SPHERES-1] = (Sphere){.position = {.y=10000, .z=-60}, .radius=10000};
+        
+        scene.s = (Vector){0,-3,20};
     }
 
-
-    for(int i = num_start_spheres; i < NUM_SPHERES-1; i++) {
-        int sphere_to_grow_from_index = floor(make_random()*i);
-
-        Vector direction = (Vector){2*make_random()-1, make_random()*-1, 2*make_random()-1};
-        direction = vector_normalize(direction);
-        Vector new_position = vector_plus(all_spheres[sphere_to_grow_from_index].position, vector_multiplyf(direction,6.0));
-        Vector the_color = make_random() > 0.6 ? color_1 : color_2;
-        all_spheres[i] = (Sphere){.position = new_position, .radius = 2.0};
-        all_colors[i] = the_color;
-    }
-
-    // Big ground sphere
-    all_colors[NUM_SPHERES-1] = (Vector){.x = 0.79, .y = 0.79, .z = 0.79};
-    all_spheres[NUM_SPHERES-1] = (Sphere){.position = {.y=10000, .z=-60}, .radius=10000};
-    
-    Vector s = {0,-3,20};
     //Vector target = {1, 0, -40};
     float xmax = 5, ymax = 5;
     int num_passes = 0;
     int step = 8;
+    FILE *file;
     while(quit == false) {
 
         while(SDL_PollEvent(&e)) {
@@ -97,34 +114,42 @@ int main(void) {
             } else if (e.type == SDL_KEYDOWN) {
                 switch (e.key.keysym.sym) {
                     case SDLK_w:
-                        s = vector_plus(s, (Vector){.z=-0.5});
-                        break;
+                        scene.s = vector_plus(scene.s, (Vector){.z=-0.5});
+                    break;
                     case SDLK_s:
-                        s = vector_plus(s, (Vector){.z=0.5});
-                        break;
+                        scene.s = vector_plus(scene.s, (Vector){.z=0.5});
+                    break;
                     case SDLK_a:
-                        s = vector_plus(s, (Vector){.x=-0.5});
-                        break;
+                        scene.s = vector_plus(scene.s, (Vector){.x=-0.5});
+                    break;
                     case SDLK_d:
-                        s = vector_plus(s, (Vector){.x=0.5});
-                        break;
+                        scene.s = vector_plus(scene.s, (Vector){.x=0.5});
+                    break;
                     case SDLK_UP:
-                        s = vector_plus(s, (Vector){.y=-0.5});
-                        break;
+                        scene.s = vector_plus(scene.s, (Vector){.y=-0.5});
+                    break;
                     case SDLK_DOWN:
-                        s = vector_plus(s, (Vector){.y=0.5});
-                        break;
+                        scene.s = vector_plus(scene.s, (Vector){.y=0.5});
+                    break;
                     case SDLK_q:
                         step = step <=1 ? 1 : step/2;
                         printf("Step: %d\n", step);
-                        break;
+                    break;
                     case SDLK_e:
                         step = step >=16 ? 16 : step*2;
                         printf("Step: %d\n", step);
-                        break;
+                    break;
                     case SDLK_p:
                         save_bmp("result.bmp", image_data, IMAGE_WIDTH, IMAGE_HEIGHT);
-                        break;
+                    break;
+                    case SDLK_b:
+                        file = fopen("data.bin", "wb");
+                        if (file) {
+                            fwrite(&scene, sizeof(scene), 1, file);
+                            fclose(file);
+                        }
+                    break;
+
                         
 
                 }
@@ -134,7 +159,7 @@ int main(void) {
                 }
             
         }
-        //double start = omp_get_wtime();
+        double start = omp_get_wtime();
 
         #pragma omp parallel for collapse(2) schedule(dynamic, 16)
         for(int screenY = 0; screenY < IMAGE_HEIGHT; screenY+=step) {
@@ -150,7 +175,7 @@ int main(void) {
                 for(int i = 0; i < numRays; i++) {
                     // bounces = 0
                     
-                    end_color = vector_plus(end_color, shoot_ray(all_spheres, all_colors, NUM_SPHERES, s, dir));
+                    end_color = vector_plus(end_color, shoot_ray(scene.all_spheres, scene.all_colors, NUM_SPHERES, scene.s, dir));
                     //printf("%f %f %f\n", end_color.x, end_color.y, end_color.z);
                 }
                 end_color = vector_dividef(end_color, (float)numRays);
@@ -169,9 +194,9 @@ int main(void) {
 
 
         }
-        //double end = omp_get_wtime();
-        //double time_taken = end-start;
-        //printf("Time: %f\n", time_taken);
+        double end = omp_get_wtime();
+        double time_taken = end-start;
+        printf("Time: %f\n", time_taken);
         num_passes++;
         #pragma omp parallel for
         for(int i = 0; i < IMAGE_WIDTH*IMAGE_HEIGHT*3; i+=3) {
